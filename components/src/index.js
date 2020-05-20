f25c9cdc2bb1319d1b4e86d19847039cd041ca1e
Paula Kusinska <paula.kusinska@gmail.com>
2020-05-20 19:45:07 +0900

    created comment section

--- components/src/index.js
+++ components/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+
 const App = () => {
   return (
-  <div className="UI container comments">
+  <div className="ui container comments">
     <div className="comment">
       <a href="/" className="avatar">
         <img alt="avatar" src={faker.image.avatar()} />
